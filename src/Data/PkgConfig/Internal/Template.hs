a67ae6540bab21199c109d913a87b7a9b3f14249
Peter Trško <peter.trsko@gmail.com>
2014-12-14 15:35:26 +0100

    Escaping of template literals
    Any character that has special meaning in pkg-config confinguration
    files, is now escaped during serialization. This way anything that gets
    in to literal template fragment is treated as is.
    
    This code is based on pkg-config code and should be consistent with
    their implementation.

--- src/Data/PkgConfig/Internal/Template.hs
+++ src/Data/PkgConfig/Internal/Template.hs
@@ -41,7 +41,7 @@ module Data.PkgConfig.Internal.Template
     )
   where
 
-import Data.Bool (otherwise)
+import Data.Bool (Bool, (||), otherwise)
 import Data.Char (Char)
 import Data.Data (Data)
 import Data.Eq (Eq((==)))
@@ -76,8 +76,67 @@ type PkgTemplate = Template
 -- | Serialize fragment in to strict 'Strict.Text'.
 fragmentToStrictText :: Fragment -> Strict.Text
 fragmentToStrictText frag = case frag of
-    Literal txt   -> txt
+    Literal txt   -> escape txt
     Variable name -> Strict.Text.pack "${" <> name <> Strict.Text.singleton '}'
+  where
+    -- There are two types of escaping in pkg-config. One is done by
+    -- read_one_line() function, that uses '\' as escape character and then
+    -- there is trim_and_sub() that treats sequence of two '$' characters as
+    -- just one and doesn't perform variable expansion. Both mentioned
+    -- functions can be found in "parse.c" file.
+    escapeChar :: Char -> Strict.Text
+    escapeChar c = Strict.Text.pack $ case c of
+        '$'  -> "$$"
+        '#'  -> "\\#"
+        '\\' -> "\\\\"
+        _    -> [c]
+
+    escape, escape' :: Strict.Text -> Strict.Text
+    escape' = Strict.Text.concatMap escapeChar
+    escape = Strict.Text.concat . escapeLoop
+
+    -- Process text by splitting it on EOL, repeatedly.
+    escapeLoop :: Strict.Text -> [Strict.Text]
+    escapeLoop txt
+      | Strict.Text.null txt = []
+      | otherwise            =
+        case Strict.Text.break isCrOrLf txt of
+            (txt1, txt2)
+              | Strict.Text.null txt2 -> [escape' txt1]
+              | otherwise           ->
+                escape' txt1 : (backslash <> eol) : escapeLoop txtRest
+              where (eol, txtRest) = processEol txt2
+
+    -- Function takes text and splits it to pair where first element is EOL
+    -- character sequence, i.e. one of "\r", "\n", "\r\n", or "\n\r". Reason
+    -- for this is that pkg-config implementation treats all this sequences as
+    -- end of line character sequence.
+    --
+    -- Input condition:
+    --   Text passed to this funtion starts with either '\r' or '\n'.
+    processEol :: Strict.Text -> (Strict.Text, Strict.Text)
+    processEol txt = case Strict.Text.splitAt 2 txt of
+        (x, y)
+          | (c1, c2) == (cr, lf) || (c1, c2) == (lf, cr) -> (x, y)
+            -- Now its one either "\r\n" or "\n\r". In either case all this
+            -- sequences are escaped the same way. Since pkg-config treats
+            -- "\r\n" and "\n\r" both as single line terminator.
+          | (c1, c2) == (cr, cr) || (c1, c2) == (lf, lf) -> (c1, c2 <> y)
+            -- Both "\r\r" and "\n\n" are two subsequent line
+            -- terminators, but at the moment it is not possible to
+            -- know if the later line terminator is not in fact
+            -- "\r\n" or "\n\r" sequence.
+          | otherwise -> (c1, c2 <> y)
+            -- Now its either '\r' or '\n' followed by non-eol character.
+          where (c1, c2) = Strict.Text.splitAt 1 x
+
+    isCrOrLf :: Char -> Bool
+    isCrOrLf c = c == '\r' || c == '\n'
+
+    backslash, cr, lf :: Strict.Text
+    backslash = Strict.Text.singleton '\\'
+    cr = Strict.Text.singleton '\r'
+    lf = Strict.Text.singleton '\r'
 
 -- | Serialize fragment in to 'Text.Builder'.
 fragmentToBuilder :: Fragment -> Text.Builder
